fb507305cf34ba73e89c5b76ea622dd8a50d4455
Benjamin Ocampo <nicolasbenjaminocampo@gmail.com>
2021-11-20 01:26:14 -0300

    Add experiments to be shown in the video

--- experiment.py
+++ experiment.py
@@ -1,8 +1,5 @@
 # %% [markdown]
 # # Integración de clustering para mejorar la clasificación de lenguaje ofensivo
-# %% [markdown]
-# ## Descripción de la tarea
-# TODO: Mencionar los experimentos y armar indices.
 # %%
 # Numpy and Pandas
 import numpy as np
@@ -14,17 +11,18 @@ import fasttext
 # Plotting
 import matplotlib.pyplot as plt
 import seaborn as sns
-from adjustText import adjust_text
-from yellowbrick.cluster import SilhouetteVisualizer, KElbowVisualizer
+from yellowbrick.cluster import KElbowVisualizer
 
-# Sklearn
+# Sklearn, XGBoost, and other models
 from sklearn.cluster import KMeans
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.preprocessing import LabelEncoder
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix, f1_score
+from sklearn.model_selection import GridSearchCV
+from xgboost import XGBClassifier
+
+# Utils
+from models import NN
+from joblib import dump
 # %% [markdown]
 # ## Exploración de datos
 # %%
@@ -35,6 +33,9 @@ train_df
 # %%
 test_df
 # %%
+sns.histplot(train_df["tweet"].apply(lambda t: len(t)))
+sns.histplot(test_df["tweet"].apply(lambda t: len(t)))
+sns.countplot(data=train_df, x="label_name")
 # %% [markdown]
 # ## Codificación de tweets
 # %%
@@ -74,178 +75,171 @@ test_df = test_df.assign(
 )
 # %% [markdown]
 # ## Clustering
-# TODO:
 # - Kmeans con ofensivos y no ofensivos
-# - Gráficos de silhouette
 # - Métricas de pureza
-# - Que hay en los clusters
-# %% [markdown]
-# ## Clasificación
-# TODO:
-# - Fasttext con todos los datos + LGR o XGBoost o NN
-# - Fasttext con datos offensivos + LGR o XGBoost o NN
-# - Lo mismo pero con undersampling
-# %% [markdown]
-# ## Clasificación con etiquetas dada por clustering
-# TODO:
-# - Solo training set con datos ofensivos
 # %%
-
-
-def plot_2DTSNE(X_TSNE,
-                expressions,
-                ax,
-                point_size=50,
-                legend_size=20,
-                tick_size=20,
-                annotation_size=20,
-                annotate=True):
-    nof_words = len(X_TSNE)
-    ax.scatter(X_TSNE[:, 0],
-               X_TSNE[:, 1],
-               s=point_size)
-    ax.set_title("t-SNE Plot", fontsize=legend_size)
-    annot_list = []
-
-    if annotate:
-        nof_words_to_annotate = 20
-        for i in np.random.randint(nof_words, size=nof_words_to_annotate):
-            a = ax.annotate(expressions[i], (X_TSNE[i, 0], X_TSNE[i, 1]),
-                            size=annotation_size)
-            annot_list.append(a)
-        adjust_text(annot_list)
-
-    ax.tick_params(axis='both', which='major', labelsize=tick_size)
-
-
-def plot_2D_kmeans(X_TSNE,
-                   km_model,
-                   ax,
-                   marker_size=2,
-                   legend_size=20,
-                   tick_size=20):
-    cluster_df = pd.DataFrame(X_TSNE)
-    cluster_df = cluster_df.assign(label=km_model.labels_)
-    sns.scatterplot(data=cluster_df, x=0, y=1, hue="label", palette="tab20", ax=ax)
-    ax.legend(bbox_to_anchor=(1.02, 1.02),
-              loc='upper left',
-              markerscale=marker_size,
-              prop={"size": legend_size})
-    ax.set_title("KMeans Plot", fontsize=legend_size)
-    ax.tick_params(axis='both', which='major', labelsize=tick_size)
-
-
-def plot_silhouette(X, km_model, ax, title_size=20, tick_size=20):
-    visualizer = SilhouetteVisualizer(km_model, colors='tab20', ax=ax)
-    visualizer.fit(X)
-    ax.set_title("Silhoutte Plot", fontsize=title_size)
-    ax.tick_params(axis='both', which='major', labelsize=tick_size)
-
-def plot_elbow(X, estimator, metric, k_range, ax, title_size=10, tick_size=10):
-    visualizer = KElbowVisualizer(estimator(),
+def plot_elbow(X,
+               metric,
+               k_range,
+               ax,
+               title="Elbow Plot",
+               title_size=10,
+               tick_size=10):
+    visualizer = KElbowVisualizer(KMeans(),
                                   k=k_range,
                                   metric=metric,
                                   timings=False,
                                   ax=ax)
     visualizer.fit(X)
-    ax.set_title("Elbow Plot", fontsize=title_size)
+    ax.set_title(title, fontsize=title_size)
     ax.tick_params(axis='both', which='major', labelsize=tick_size)
+
+_, axes = plt.subplots(1, 2, figsize=(10, 5))
+plot_elbow(X=np.vstack(train_df["vec_off"]),
+           k_range=(2, 10),
+           metric="distortion",
+           title="Elbow with offensive data",
+           ax=axes[0])
+plot_elbow(X=np.vstack(train_df["vec_mixed"]),
+           k_range=(2, 10),
+           metric="distortion",
+           title="Elbow with mixed data",
+           ax=axes[1])
+plt.show()
+plt.clf()
 # %%
-model = fasttext.train_unsupervised(corpus_file,
-                                    model="cbow",
-                                    lr=0.3,
-                                    epoch=100,
-                                    dim=100,
-                                    wordNgrams=4,
-                                    ws=4)
+kmeans_off = KMeans(n_clusters=4)
+kmeans_off.fit(np.vstack(train_df["vec_off"]))
+
+kmeans_mixed = KMeans(n_clusters=5)
+kmeans_mixed.fit(np.vstack(train_df["vec_mixed"]))
+
+train_df = train_df.assign(cluster_off=kmeans_off.labels_)
+train_df = train_df.assign(cluster_mixed=kmeans_mixed.labels_)
+
+test_df = test_df.assign(
+    cluster_off=kmeans_off.predict(np.vstack(test_df["vec_off"])))
+test_df = test_df.assign(
+    cluster_mixed=kmeans_mixed.predict(np.vstack(test_df["vec_mixed"])))
 # %%
-train_df["vec"] = train_df["tweet"].apply(lambda t: model.get_sentence_vector(t))
-test_df["vec"] = train_df["tweet"].apply(lambda t: model.get_sentence_vector(t))
-X_train, y_train = np.vstack(train_df["vec"]), train_df["labels"]
-X_test, y_test = np.vstack(test_df["vec"]), test_df["labels"]
+pd.crosstab(train_df["label"], train_df["cluster_off"])
 # %%
-kmeans = KMeans(n_clusters=20)
-kmeans.fit(X_train)
+pd.crosstab(train_df["label"], train_df["cluster_mixed"])
+# %% [markdown]
+# ## Clasificación
+# - Fasttext con todos los datos + LGR o XGBoost o NN
+# - Fasttext con datos offensivos + LGR o XGBoost o NN
 # %%
-train_df = train_df.assign(cluster=kmeans.labels_)
+runs = [
+     (LogisticRegression(), {
+         "class_weight": ["balanced"],
+         "penalty": ["elasticnet"],
+         "C": [1, 0.01],
+         "solver": ["saga"],
+         "max_iter": [1000],
+         "l1_ratio": [0, 1, 0.5]
+     }),
+     (XGBClassifier(), {
+         "objective": ["binary:logistic"],
+         "n_estimators": [500],
+         "gamma": [0.001, 1],
+         "max_depth": [20],
+         "booster": ["gbtree"],
+         "eval_metric": ["logloss"],
+         "use_label_encoder": [False],
+         "lambda": [0.001, 1]
+     }),
+    (NN(), {
+        "h_size": [32, 64],
+        "n_layers": [4, 8],
+        "bn_bool": [False],
+        "p": [0.2],
+        "epochs": [20],
+        "batch_size": [32],
+        "lr": [0.001],
+        "weight_decay": [0]
+    }),
+]
+vec_names = ["vec_off", "vec_mixed"]
+experiments = []
+for vec_name in vec_names:
+    X_train, y_train = np.vstack(
+        train_df[vec_name]), train_df["label"].to_numpy()
+    X_test, y_test = np.vstack(test_df[vec_name]), test_df["label"].to_numpy()
+
+    results = { }
+    for model, grid in runs:
+        clf = GridSearchCV(
+            estimator=model,
+            param_grid=grid,
+            scoring="f1",
+            refit=True,
+            cv=5,
+            verbose=3
+        )
+        clf.fit(X_train, y_train)
+        y_pred = clf.best_estimator_.predict(X_test)
+        results["model_name"] = model.__class__.__name__
+        results["vector"] = vec_name
+        results["params"] = clf.best_params_
+        results["score"] = f1_score(y_test, y_pred)
+        results["pred"] = y_pred
+        experiments.append(results)
+        dump(clf.best_estimator_, f"{model.__class__.__name__}__{vec_name}.joblib")
+
+experiments_df = pd.DataFrame(experiments)
+experiments_df.to_csv("results.csv", index=False)
 # %%
-pd.crosstab(train_df["subtask_a"], train_df["cluster"])
+experiments_df.assign(cm=experiments_df["pred"].apply(
+    lambda pred: confusion_matrix(y_test, pred)))
+nof_experiments = len(experiments_df)
+
+best_results_df = experiments_df.iloc[experiments_df["score"].argmax()]
+
+cm = confusion_matrix(y_test, best_results_df["pred"])
+cm = cm / np.sum(cm, axis=1).reshape(-1, 1)
+sns.heatmap(cm, annot=True)
+plt.title(f"{best_results_df['model_name']} {best_results_df['vector']}")
+plt.xlabel("Predicted label")
+plt.ylabel("True label")
+plt.show()
+plt.clf()
+# %% [markdown]
+# ## Clasificación con etiquetas dada por clustering
+# - Solo training set con datos ofensivos
 # %%
-clf = LogisticRegression(class_weight="balanced")
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-cm = confusion_matrix(y_test, y_pred)
-sns.heatmap(cm, annot=True, fmt="d")
+train_off_df = train_df[train_df["label_name"] == "OFF"]
+test_off_df = test_df[test_df["label_name"] == "OFF"]
+
+model = NN()
+params = {
+    "h_size": 64,
+    "n_layers": 8,
+    "bn_bool": False,
+    "p": 0.2,
+    "epochs": 20,
+    "batch_size": 32,
+    "lr": 0.001,
+    "weight_decay": 0
+}
+
+X_train_off, y_train_off = np.vstack(
+    train_off_df["vec_off"]), train_off_df["cluster_off"].to_numpy()
+X_test_off, y_test_off = np.vstack(
+    test_off_df["vec_off"]), test_off_df["cluster_off"].to_numpy()
+
+model.set_params(**params)
+model.fit(X_train_off, y_train_off)
+y_pred_off = model.predict(X_test_off)
+
+cm = confusion_matrix(y_test_off, y_pred_off)
+cm = cm / np.sum(cm, axis=1).reshape(-1, 1)
+sns.heatmap(cm, annot=True)
+plt.title(f"NN only_off")
 plt.xlabel("Predicted label")
 plt.ylabel("True label")
 plt.show()
 plt.clf()
 
-print(classification_report(y_test, y_pred))
-# %%
-sns.histplot(train_df["tweet"].apply(lambda t: len(t)))
-# %%
-sns.countplot(data=train_df, x="subtask_a")
-# %%
-train_off_df = train_df[train_df["subtask_a"] == "OFF"]
-train_not_df = train_df[train_df["subtask_a"] == "NOT"]
-# %%
-sentences = train_off_df["tweet"]
-corpus_file = "tweets.txt"
-model = fasttext.train_unsupervised(corpus_file,
-                                    model="cbow",
-                                    lr=0.3,
-                                    epoch=100,
-                                    dim=100,
-                                    wordNgrams=4,
-                                    ws=4)
-# %%
-train_off_df = train_off_df.assign(vec_off=train_off_df["tweet"].apply(
-    lambda t: model.get_sentence_vector(t)))
-train_not_df = train_not_df.assign(vec_off=train_not_df["tweet"].apply(
-    lambda t: model.get_sentence_vector(t)))
-train_new_df = pd.concat([train_off_df, train_not_df])
-X_train_new, y_train_new = np.vstack(
-    train_new_df["vec_off"]), train_new_df["labels"]
-
-test_off_df = test_df[test_df["subtask_a"] == "OFF"]
-test_not_df = test_df[test_df["subtask_a"] == "NOT"]
-
-test_off_df = test_off_df.assign(
-    vec_off=test_off_df["tweet"].apply(lambda t: model.get_sentence_vector(t)))
-test_not_df = test_not_df.assign(
-    vec_off=test_not_df["tweet"].apply(lambda t: model.get_sentence_vector(t)))
-test_new_df = pd.concat([test_off_df, test_not_df])
-X_test_new, y_test_new = np.vstack(test_new_df["vec_off"]), test_new_df["labels"]
-
-clf = SVC(class_weight="balanced")
-clf.fit(X_train_new, y_train_new)
-
-y_pred = clf.predict(X_test_new)
-print(classification_report(y_test_new, y_pred))
-# %%
-cm = confusion_matrix(y_test_new, y_pred)
-sns.heatmap(cm, annot=True, fmt="d")
-plt.xlabel("y_pred")
-plt.ylabel("y_true")
-plt.show()
-plt.clf()
-# %%
-test_off_df = test_off_df.assign(
-    new_label=kmeans_off.predict(np.vstack(test_off_df["vec_off"])))
-# %%
-test_off_df
-# %%
-X_test_off, y_test_off = np.vstack(test_off_df["vec_off"]), test_off_df["new_label"]
-# %%
-y_pred_off = clf.predict(X_test_off)
-# %%
-print(classification_report(y_test_off, y_pred_off))
-# %%
-test_off_df[test_off_df["new_label"] == 0]
-# %%
-test_off_df[test_off_df["new_label"] == 1]
-# %%
-train_not_df = train_not_df.assign(
-    new_label=10)
+dump(model, f"{model.__class__.__name__}__vec_off__off.joblib")
 # %%
